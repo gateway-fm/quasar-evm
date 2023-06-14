1ce26046c772e40cfd9079e1b365a76c88054a8e
Andrey Skurlatov <a.skurlatov@gmail.com>
2023-06-14 19:10:39 +0200

    Initial commit

---
+++ config/scheme.go
@@ -0,0 +1,8 @@
+package config
+
+// Scheme represents the application configuration scheme.
+type Scheme struct {
+	// Env is the application environment.
+	Env string
+	// TODO add needed config params
+}
