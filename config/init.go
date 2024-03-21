353274ed17e0509c1709cabcab66a24935f392c4
asolovov <andre.solovov@gmail.com>
2024-03-21 18:47:42 +0300

    feat: logic and readme

--- config/init.go
+++ config/init.go
@@ -9,5 +9,21 @@ func init() {
 	// environment - could be "local", "prod", "dev"
 	viper.SetDefault("env", "prod")
 
-	// TODO add default values for all configuration fields
+	viper.SetDefault("tokens", []string{
+		"ADA", "ALGO", "ARB",
+		"AVAX", "BNB", "BTC",
+		"DOGE", "ETH", "FIL",
+		"FLR", "LTC", "MATIC",
+		"SOL", "USDC", "USDT",
+		"XDC", "XLM", "XRP",
+	})
+
+	viper.SetDefault("ws.url", "wss://oracle.gateway.fm")
+	viper.SetDefault("ws.WriteFrequencyMS", 5000)
+
+	viper.SetDefault("web3.SendFrequencyMS", 10000)
+	viper.SetDefault("web3.RpcURL", "")
+	viper.SetDefault("web3.SignerPK", "")
+	viper.SetDefault("web3.ChainID", 0)
+	viper.SetDefault("web3.OracleAddress", "")
 }
